003755a1f2eed3e3b66deb7d0151b3a0d9554410
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-01-25 19:34:50 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+
+function superbowlWin(objects) {
+    const element = objects.find(obj => (obj.result === "W") ? obj : undefined);
+    return (element === undefined) ? element : element.year;
+}
